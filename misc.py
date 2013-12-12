1bf5f553fe04bb1deede7762050e016a32fb56bb
eeue56 <eeue56@users.noreply.github.com>
2013-12-12 00:01:59 +0000

    Create misc.py

---
+++ misc.py
@@ -0,0 +1,12 @@
+COLOURS = { 'black' : (0, 0, 0),
+            'other-grey' : (0.25, 0.25, 0.25),
+            'grey' : (0.4, 0.4, 0.4),
+            'red' :  (255, 0, 0),
+            'white' : (1, 1, 1)}
+
+DIRECTIONS = {
+        'up' : 1,
+        'down' : 2,
+        'left' : 30,
+        'right' : 40
+}
