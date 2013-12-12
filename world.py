ddca9f86aac8949a05313fac34c3ee5730a1aea9
eeue56 <eeue56@users.noreply.github.com>
2013-12-12 01:51:54 +0000

    Update world.py

--- world.py
+++ world.py
@@ -1,12 +1,15 @@
 from __future__ import division
 
 from misc import *
+from global_exceptions import *
 
 class World(object):
 
-    def __init__(self, player, height=96, width=96):
+    def __init__(self, player, height=100, width=100):
         self.player = player
         self.objects = []
+        self.height = height
+        self.width = width
 
         self.object_array = [[None for x in xrange(width)] for y in xrange(height)]
 
@@ -16,10 +19,17 @@ class World(object):
         for object_ in objects:
             self.object_array[object_.y][object_.x] = object_
 
+    def add_object(self, object_):
+        self.object.append(object_)
+        self.object_array[object_.y][object_.x] = object_
+
     def is_object_there(self, x, y):
         return self.object_array[y][x] is not None
 
     def is_going_to_collide(self, old_x, old_y, new_x, new_y, object_):
+        if new_x > self.width or new_x < 0 or new_y > self.height or new_y < 0:
+            raise OutOfWorldException
+
         if old_x > new_x:
             old_x, new_x = new_x, old_x
 
@@ -49,52 +59,72 @@ class World(object):
     def _move_object(self, object_, x=0, y=0):
         self._move(object_.x, object_.y, object_.x + x, object_.y + y)
 
-    def move(self, x, y, direction, distance=1):
-        object_ = self.object_array[y][x]
-
+    def _dirty_move(self, object_, direction, distance):
         if direction == DIRECTIONS['up']:
             for _ in [1 for _ in xrange(distance)]:
                 if self.is_object_going_to_collide(object_, y=1):
-                    self._move_object(object_, y=1)
+                    raise CollisionException(self.object_array[object_.y + 1][object_.x])
+                self._move_object(object_, y=1)
 
         elif direction == DIRECTIONS['down']:
             for _ in [1 for _ in xrange(distance)]:
                 if self.is_object_going_to_collide(object_, y=-1):
-                    self._move_object(object_, y=-1)
+                    raise CollisionException(self.object_array[object_.y - 1][object_.x])
+                self._move_object(object_, y=-1)
 
         elif direction == DIRECTIONS['left']:
             for _ in [1 for _ in xrange(distance)]:
                 if self.is_object_going_to_collide(object_, x=-1):
-                    self._move_object(object_, x=-1)
+                    raise CollisionException(self.object_array[object_.y][object_.x - 1])
+                self._move_object(object_, x=-1)
 
         elif direction == DIRECTIONS['right']:
             for _ in [1 for _ in xrange(distance)]:
                 if self.is_object_going_to_collide(object_, x=1):
-                    self._move_object(object_, x=1)
-
+                    raise CollisionException(self.object_array[object_.y][object_.x + 1])
+                self._move_object(object_, x=1)
 
         elif direction == DIRECTIONS['up'] + DIRECTIONS['left']:
             for _ in [1 for _ in xrange(distance)]:
                 if self.is_object_going_to_collide(object_, x=1, y=-1):
-                    self._move_object(object_, x=1, y=-1)
+                    raise CollisionException(self.object_array[object_.y - 1][object_.x + 1])
+                self._move_object(object_, x=1, y=-1)
 
         elif direction == DIRECTIONS['up'] + DIRECTIONS['right']:
             for _ in [1 for _ in xrange(distance)]:
                 if self.is_object_going_to_collide(object_, x=1, y=1):
-                    self._move_object(object_, x=1, y=1)
+                    raise CollisionException(self.object_array[object_.y + 1][object_.x + 1])
+                self._move_object(object_, x=1, y=1)
 
         elif direction == DIRECTIONS['down'] + DIRECTIONS['left']:
             for _ in [1 for _ in xrange(distance)]:
                 if self.is_object_going_to_collide(object_, x=-1, y=-1):
-                    self._move_object(object_, x=-1, y=-1)
+                    raise CollisionException(self.object_array[object_.y - 1][object_.x - 1])
+                self._move_object(object_, x=-1, y=-1)
 
         elif direction == DIRECTIONS['down'] + DIRECTIONS['right']:
             for _ in [1 for _ in xrange(distance)]:
                 if self.is_object_going_to_collide(object_, x=-1, y=1):
-                    self._move_object(object_, x=-1, y=1)
+                    raise CollisionException(self.object_array[object_.y + 1][object_.x - 1])
+                self._move_object(object_, x=-1, y=1)
+
+    def move(self, x, y, direction, distance=1):
+        object_ = self.object_array[y][x]
+        self._dirty_move(object_, direction, distance)       
 
     def move_object(self, object_, direction, distance=1):
         self.move(object_.x, object_.y, direction, distance)
 
     def move_player(self, direction, distance=1):
         self.move_object(self.player, direction, distance)
+
+    def remove_object(self, object_):
+        self.object_array[object_.y][object_.x] = None
+
+    def draw(self):
+        for object_ in self.objects:
+            object_.draw()
+
+    def tick(self):
+        for object_ in self.objects:
+            object_.tick()
