0582deeac237b98020bcff211385ba00cd165e40
eeue56 <eeue56@users.noreply.github.com>
2013-12-12 04:12:25 +0000

    Update world.py

--- world.py
+++ world.py
@@ -30,30 +30,23 @@ class World(object):
     def is_object_there(self, x, y):
         return self.object_array[y][x] is not None
 
-    def is_going_to_collide(self, old_x, old_y, new_x, new_y, object_):
-        if new_x >= self.width or new_x < 0 or new_y >= self.height or new_y < 0:
-            raise OutOfWorldException
+    def is_going_to_collide(self, new_object):
+        
+        for (x, y) in new_object.populated_squares:
+            if y < 0 or y >= self.height or x < 0 or x >= self.width:
+                raise OutOfWorldException
 
-        if old_x > new_x:
-            old_x, new_x = new_x, old_x
-
-        if old_y > new_y:
-            old_y, new_y = new_y, old_y
-
-        for x in xrange(old_x + 1, new_x + 1):
-            for y in xrange(old_y + 1, new_y + 1):
-                if x == object_.x and y == object_.y:
-                    continue
-
-                if self.object_array[y][x] is not None:
-                    self._last_collide = self.object_array[y][x]
-                    return True
+            if self.object_array[y][x] is not None:
+                self._last_collide = self.object_array[y][x]
+                return True
         return False
 
     def is_object_going_to_collide(self, object_, x=0, y=0):
-        return self.is_going_to_collide(object_.x, object_.y, 
-            object_.x + x, object_.y + y,
-            object_)
+        new_object = copy(object_)
+        new_object.x += x
+        new_object.y += y
+
+        return self.is_going_to_collide(new_object)
 
     def _move(self, old, new, object_):
 
